2e8003880297e20e48c747c14d38427c97aeff5b
lfisher447-afk <lfisher447@gmail.com>
2026-04-29 03:50:47 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,27 @@
-import withPWAInit from 'next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
+  scope: "/",
+  sw: "sw.js",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: { domains: ['image.tmdb.org', 'api.dicebear.com'] }
+  reactStrictMode: true,
+  images: {
+    remotePatterns:[
+      { protocol: "https", hostname: "image.tmdb.org" },
+      { protocol: "https", hostname: "api.dicebear.com" }
+    ]
+  }
 };
 
 export default withPWA(nextConfig);
